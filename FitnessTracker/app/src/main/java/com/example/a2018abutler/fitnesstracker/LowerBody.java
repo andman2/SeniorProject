bf65350fc95f526d0c75613586a4e57034210736
andman2 <andman.b2000@gmail.com>
2017-11-28 09:35:17 -0500

    added two activities

---
+++ FitnessTracker/app/src/main/java/com/example/a2018abutler/fitnesstracker/LowerBody.java
@@ -0,0 +1,13 @@
+package com.example.a2018abutler.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LowerBody extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lower_body);
+    }
+}
--- FitnessTracker/app/src/main/java/com/example/a2018abutler/fitnesstracker/MainActivity.java
+++ FitnessTracker/app/src/main/java/com/example/a2018abutler/fitnesstracker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a2018abutler.fitnesstracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton upper = (ImageButton) findViewById(R.id.UpperBody);
-        ImageButton lower = (ImageButton) findViewById(R.id.LowerBody);
+        ImageButton lower = (ImageButton) findViewById(R.id.LowerBody);//image button to go to lower body workout activity
+
+        lower.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                switchActivity(0);
+            }
+
+        });
+    }
+    public void switchActivity(int x){//this method allows usd to switch activities (pass 0 for lower body and anything else for upper)
+        Intent intent;
+        if (x == 0){
+            intent = new Intent(this, LowerBody.class);
+        }
+        else {
+            intent = new Intent(this, UpperBody.class);
+        }
+        startActivity(intent);
     }
 }
