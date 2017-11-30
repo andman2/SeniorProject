86821d8602e7d1cb7eb56685d8f9cff0c7fbf2c4
andman2 <andman.b2000@gmail.com>
2017-11-30 10:15:52 -0500

    changed layouts started menu

--- FitnessTracker/app/src/main/java/com/example/a2018abutler/fitnesstracker/MainActivity.java
+++ FitnessTracker/app/src/main/java/com/example/a2018abutler/fitnesstracker/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.a2018abutler.fitnesstracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageButton upper = (ImageButton) findViewById(R.id.UpperBody);
         ImageButton lower = (ImageButton) findViewById(R.id.LowerBody);//image button to go to lower body workout activity
+
 
         lower.setOnClickListener(new View.OnClickListener() {
 
@@ -31,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
     }
     public void switchActivity(int x){//this method allows usd to switch activities (pass 0 for lower body and anything else for upper)
         Intent intent;
