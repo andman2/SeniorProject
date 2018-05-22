71d610b65a8d871d833a5fa89fdc42d2c1b87d52
andman2 <andman.b2000@gmail.com>
2018-05-22 10:18:39 -0400

    worked on file write

--- FitnessTracker/app/src/main/java/com/example/a2018abutler/fitnesstracker/inputActivity.java
+++ FitnessTracker/app/src/main/java/com/example/a2018abutler/fitnesstracker/inputActivity.java
@@ -1,19 +1,44 @@
 package com.example.a2018abutler.fitnesstracker;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 public class inputActivity extends AppCompatActivity {
     public EditText weightin, repin;
     public Spinner excer;
+    public Button ent;
+    public int wei;
+    public int rep;
+    public String ex;
+    public Context context;
+    public CharSequence text = "Input Weight";
+    public CharSequence text2 = "Input Repetitions";
+    public CharSequence text3 = "Pick A Lift";
+    public CharSequence text4 = "Data Saved";
+    public int duration = Toast.LENGTH_SHORT;
+    public boolean sp = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+        context  = getApplicationContext();
         weightin = (EditText) findViewById(R.id.weightin);
         repin = (EditText) findViewById(R.id.repin);
         excer =(Spinner) findViewById(R.id.liftSpinner);
@@ -21,5 +46,90 @@ public class inputActivity extends AppCompatActivity {
                 R.array.lifts_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         excer.setAdapter(adapter);
+        excer.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ex = getResources().getStringArray(R.array.lifts_array)[position];
+                sp = true;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                sp =false;
+            }
+        });
+        ent = (Button) findViewById(R.id.ent);
+        ent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sp == true) {
+                    if (weightin.getText() != null) {
+                        wei = Integer.parseInt(weightin.getText().toString());
+
+                        if (repin.getText() != null) {
+                            rep = Integer.parseInt(repin.getText().toString());
+                            calcAndSave(wei,rep,ex);
+                            weightin.getText().clear();
+                            repin.getText().clear();
+                            Toast toast = Toast.makeText(context, text4, duration);
+                            toast.show();
+                        } else {
+                            Toast toast = Toast.makeText(context, text2, duration);
+                            toast.show();
+                        }
+                    } else {
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+
+                }
+                else{
+                    Toast toast = Toast.makeText(context, text3, duration);
+                    toast.show();
+                }
+            }
+        });
     }
+    public void calcAndSave(int w, int r, String ex){
+        int pscore = (int)(w/(1.0278-0.0278 *r));
+
+        Calendar calendar = Calendar.getInstance();
+        Date d = calendar.getTime();
+        boolean isF = false;
+        File directory = getDir("myStuff",MODE_PRIVATE);
+        String[] files = fileList();
+        for (int x = 0; x < files.length; x++){
+            if(files[x].equals(ex)){
+                isF = true;
+            }
+        }
+        if(isF){
+            try {
+                String strText = "-" + d.toString() + "," +pscore ;
+
+                // MODE_APPEND, MODE_WORLD_READABLE, MODE_WORLD_WRITEABLE
+                // append to file
+                FileOutputStream fos = openFileOutput(ex, getApplicationContext().MODE_APPEND);
+
+                fos.write(strText.getBytes());
+                fos.close();
+            } catch (IOException e) {
+                e.toString();
+            }
+
+        }
+        else{
+            FileOutputStream outputStream;
+            String strText = d.toString() + "," +pscore ;
+            try {
+                outputStream = openFileOutput(ex, Context.MODE_PRIVATE);
+                outputStream.write(strText.getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
